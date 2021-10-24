9f9dd189692465f6646e8b8f4bcca9ee86e146c6
André Krosby <andre.krosby92@icloud.com>
2021-10-24 15:51:41 +0200

    fix: add api key env variable

--- pages/kontakt/index.tsx
+++ pages/kontakt/index.tsx
@@ -3,13 +3,15 @@ import dynamic from 'next/dynamic'
 const ContactSection = dynamic(() => import('scenarios/contact/components/Contact'))
 
 export default function Contact(): JSX.Element {
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_EMBED_API_KEY
+
   return (
     <div>
       <iframe
         allowFullScreen
         height="350"
         loading="lazy"
-        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJV4RNpQjjQ0YRfWKwKlI_ccM&key=AIzaSyCMWhrk--J353RIQ5kQ4qtXCHyDKZejxs8&language=nb"
+        src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJV4RNpQjjQ0YRfWKwKlI_ccM&key=${apiKey}&language=nb`}
         style={{ border: '0' }}
         width="100%"
       />

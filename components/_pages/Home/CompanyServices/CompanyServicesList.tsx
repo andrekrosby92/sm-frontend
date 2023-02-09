989e8f8d71894c16097930c0d72a6d8429d2ed9f
André Krosby <andre.krosby92@icloud.com>
2023-02-09 23:16:56 +0100

    temp remove bird-free from homepage

--- components/_pages/Home/CompanyServices/CompanyServicesList.tsx
+++ components/_pages/Home/CompanyServices/CompanyServicesList.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react'
+
 import SectionHeaderPrimary from 'components/SectionHeaders/SectionHeaderPrimary'
 import SectionWrapperPrimary from 'components/SectionWrappers/SectionWrapperPrimary'
 import { CompanyServiceMinimal } from 'types/company-service'
@@ -13,15 +15,21 @@ export default function CompanyServicesList({
     <SectionWrapperPrimary className="space-y-6">
       <SectionHeaderPrimary>Våre tjenester</SectionHeaderPrimary>
       <section className="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
-        {companyServices.map((elem) => (
-          <CompanyServiceItem
-            icon={elem.icon}
-            key={elem.title}
-            shortDescription={elem.shortDescription}
-            slug={elem.slug.current}
-            title={elem.title}
-          />
-        ))}
+        {companyServices.map((elem) => {
+          if (elem.slug.current === 'bird-free') {
+            return <Fragment />
+          }
+
+          return (
+            <CompanyServiceItem
+              icon={elem.icon}
+              key={elem.title}
+              shortDescription={elem.shortDescription}
+              slug={elem.slug.current}
+              title={elem.title}
+            />
+          )
+        })}
       </section>
     </SectionWrapperPrimary>
   )

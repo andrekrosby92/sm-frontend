47c722dd92eebe7a3df8aae69acb94aef0b66e2d
André Krosby <andre.krosby92@icloud.com>
2023-02-10 10:16:26 +0100

    fix typescript error

--- components/Sanity/serializers.tsx
+++ components/Sanity/serializers.tsx
@@ -26,8 +26,10 @@ export const serializers = {
   },
 
   marks: {
-    link: ({ mark, children }: { mark: { blank: boolean; href: string }; children: ReactNode }): JSX.Element => {
+    link: (props: unknown): JSX.Element => {
+      const { mark, children } = props as { mark: { blank: boolean; href: string }; children: ReactNode }
       const { blank, href } = mark
+
       return blank ? (
         <a href={href} rel="noopener noreferrer" target="_blank">
           {children}
